ebd0794aa286a64daa82167f26ae80cb8540a99c
omaralashqar-svg <55415252+omaralashqar-svg@users.noreply.github.com>
2021-09-17 23:35:48 -0500

    Add files via upload

---
+++ BasicOutputwithVariables.py
@@ -0,0 +1,16 @@
+print('Enter integer:')
+userNum1 = int(input())
+print("You entered:", userNum1)
+
+userNumSquared = userNum1 * userNum1
+userNumCubed = userNum1 * userNum1 * userNum1
+print(userNum1, 'squared is', userNumSquared)
+
+print('And', userNum1, 'cubed is', userNumCubed, '!!')
+
+print('Enter another integer:')
+user_num2 = int(input())
+userSum = userNum1 + user_num2
+print(userNum1, "+", user_num2, 'is', userSum)
+userProd = userNum1 * user_num2
+print(userNum1, '*', user_num2, 'is', userProd)
